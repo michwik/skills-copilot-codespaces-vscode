ee547b7e7278048a07bbccc998f3b4ed7ac97eba
Michelle Wiktorsson <132301232+michwik@users.noreply.github.com>
2023-08-07 17:54:02 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,9 @@
+function skillsMember() {
+    var member = document.getElementById("member");
+    var skills = document.getElementById("skills");
+    var skillsMember = document.getElementById("skillsMember");
+
+    member.style.display = "none";
+    skills.style.display = "block";
+    skillsMember.style.display = "block";
+}
